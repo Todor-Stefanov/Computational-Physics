dc0b939ea0abcdff77857f7d97bcddb0254e175c
Todor-Stefanov <Tistefanov@students.softuni.bg>
2022-10-17 18:08:59 +0300

    Correcting the file. Comparing two methods: 1. Direct Summing and 2. Ewjen's Method.

--- Madelung constant/Madelung constant/Madelung constant.cpp
+++ Madelung constant/Madelung constant/Madelung constant.cpp
@@ -3,10 +3,22 @@
 using namespace std;
 
 
-double MadelungConst(float i) {
+double directSumming(double i) {
 	double sumPart = 0;
 	int n = 1;
-	while (i < (1 / (n * (n + 1)))) {
+	while (i <= (static_cast<double>(1) / n)) {
+		sumPart += pow(-1, n + 1) / n;
+		n++;
+	}
+	cout << "Number of cycles: " << n << endl;
+	return 2 * sumPart;
+}
+
+
+double ewjensMethod(double i) {
+	double sumPart = 0;
+	int n = 1;
+	while (i <= (1 / pow(n, 2))) {
 		sumPart += pow(-1, n + 1) / (n * (n + 1));
 		n++;
 	}
@@ -16,6 +28,7 @@ double MadelungConst(float i) {
 
 int main()
 {
-	cout << "The result is: " << MadelungConst(0.001) << endl;
+	cout << "The result is: " << directSumming(0.001) << endl;
+	cout << "The result is: " << ewjensMethod(0.001) << endl;
 	return 0;
 }
